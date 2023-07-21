99b3c9f753007ea70e05015325f0c582f2df4f26
touredri <dcompte223@gmail.com>
2023-07-21 11:18:11 +0000

    setup

---
+++ src/test/java/com/quiz/quizapi/QuizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.quizapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
